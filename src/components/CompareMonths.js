ca0cfb671fb8fe797c070a857b86cc4eb32dd8d2
jeff-burns <1jeffreyburns@gmail.com>
2018-11-20 19:29:01 -0700

    added styling and gridding of data to compare by month component

--- src/components/CompareMonths.js
+++ src/components/CompareMonths.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import { FlatList, View, Text } from 'react-native';
+import { FlatList, View, Text, StyleSheet, Dimensions } from 'react-native';
 
 const data = [
-    { key: 'A'}, { key: 'B'}, { key: 'C'}, { key: 'D'}, { key: 'E'}, { key: 'F'}, { key: 'G'}, { key: 'H'}, { key: 'I'}, { key: 'J'}, { key: 'K'}, { key: 'L'}
+    { key: ''}, { key: 'Nov'}, { key: 'Oct'}, { key: 'Sept'}, { key: 'Total Credit'}, { key: 'F'}, { key: 'G'}, { key: 'H'}, { key: 'Total Debit'}, { key: 'J'}, { key: 'K'}, { key: 'L'}, { key: 'Credit /Day'}, { key: 'N'}, { key: 'O'}, { key: 'P'}, { key: 'Debit /Day'}, { key: 'R'}, { key: 'S'}, { key: 'T'}
 ];
 
-const numColumns = 3;
+const numColumns = 4;
 
 class CompareMonths extends Component {
 
     renderItem = ({ item }) => {
         return (
-            <View>
-                <Text>
+            <View style={styles.item}>
+                <Text style={styles.itemText}>
                     {item.key}
                 </Text>
             </View>
@@ -23,11 +23,33 @@ class CompareMonths extends Component {
         return (
             <FlatList 
                 data={data}
+                style={styles.container}
                 renderItem={this.renderItem}
                 numColumns={numColumns}
             />
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginVertical: 20,
+    },
+    item: {
+      backgroundColor: '#4D243D',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flex: 1,
+      margin: 1,
+      height: Dimensions.get('window').width / numColumns, // approximate a square
+    },
+    itemInvisible: {
+      backgroundColor: 'transparent',
+    },
+    itemText: {
+      color: '#fff',
+    },
+  });
 
 export default CompareMonths;
